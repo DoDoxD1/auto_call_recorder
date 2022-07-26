68331147a01e4de8336175311fa945424ad58c9c
aunuj <aunujain53@gmail.com>
2022-07-27 01:29:01 +0530

    ui done

--- app/src/main/java/com/example/autocallrecorder/MainActivity.java
+++ app/src/main/java/com/example/autocallrecorder/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.autocallrecorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    private boolean isRecording = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //references
+        button = findViewById(R.id.record_button);
+
+        button.setOnClickListener(view -> {
+            if (!isRecording){
+                isRecording = true;
+                Toast.makeText(this, "Recording Started!", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                isRecording = false;
+                Toast.makeText(this, "Recording Stopped!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
